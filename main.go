457f84e16417d80c15a8fef8fad0d48016d4f832
Kevin <accounts_kkla@icloud.com>
2018-07-07 22:33:24 +0200

    Lambda raus
    Lambda rausgeworfen und die Abfrage nach dem SpotifyToken hinzugefügt

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"log"
 
-	"github.com/aws/aws-lambda-go/lambda"
+	"./refreshToken"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
-}
-
 func main() {
-	lambda.Start(HandleRequest)
+	err := refreshToken.RefreshToken()
+	log.Fatal(err)
 }
